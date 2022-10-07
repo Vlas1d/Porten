8316f21e8d014067dca282c8129c02b0640fa7cb
Vlas1d <114921256+Vlas1d@users.noreply.github.com>
2022-10-07 03:09:23 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+const swiper = new Swiper('.div-scroll', {
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: "cards",
+    grabCursor: true,
+
+});
+
+var swiper1 = new Swiper(".div-scroll1", {
+
+    slidesPerView: (screen.availWidth >= 1200) ? 4 : 
+    (screen.availWidth < 1200 && screen.availWidth >= 768) ? 3 :
+    (screen.availWidth < 768 && screen.availWidth >= 544) ? 2 : 1, 
+
+    spaceBetween: 25,
+    freeMode: true,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport:true,
+        pageUpDown:true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    grabCursor: true,
+});
+
+const swiper2 = new Swiper('.div-scroll2', {
+
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 1,
+
+    autoplay: {
+        delay: 3000,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    grabCursor: true,
+});
