2783d864d7b238fdf0227c4a6d37660e845efa03
Vlas1d <114921256+Vlas1d@users.noreply.github.com>
2022-10-07 03:36:47 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,30 @@ const swiper = new Swiper('.div-scroll', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    effect: "cards",
     grabCursor: true,
 
 });
 
 var swiper1 = new Swiper(".div-scroll1", {
-
+/*
     slidesPerView: (screen.availWidth >= 1200) ? 4 : 
     (screen.availWidth < 1200 && screen.availWidth >= 768) ? 3 :
     (screen.availWidth < 768 && screen.availWidth >= 544) ? 2 : 1, 
+*/
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        544: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+    },
 
     spaceBetween: 25,
     freeMode: true,
